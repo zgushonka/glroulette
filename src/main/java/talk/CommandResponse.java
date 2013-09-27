11fb2200ed9f983b51626dd77c66ce263c0aacd4
zgushonka <zgushonka@gmail.com>
2013-09-27 13:07:36 +0300

    response update

--- src/main/java/talk/CommandResponse.java
+++ src/main/java/talk/CommandResponse.java
@@ -3,7 +3,7 @@ package talk;
 public class CommandResponse extends Response {
 	
 	public CommandResponse (String userid, String command, String answer) {
-		super (userid, command, answer);
+		super (userid, command, answer, "");
 	}
 	
 	
