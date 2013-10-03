8afb836199215579335826006655ce6ecc9f43ac
nomadrain <nomadrain@gmail.com>
2013-10-03 15:26:02 +0300

    Changed performGameMove

--- src/main/java/casino/Croupie.java
+++ src/main/java/casino/Croupie.java
@@ -12,15 +12,15 @@ import org.nr.roulette.exceptions.ValidationException;
 
 import bets.Bet;
 
-
 public class Croupie {
-    
+
     private static Croupie myOwnCroupieWithBlackJack = null;
     private static Integer manualSpinNumber = null;
     private static Roulette roulette = null;
-    
-    private Croupie() {}
-    
+
+    private Croupie() {
+    }
+
     // Singleton
     public synchronized static Croupie newInstance() {
         if (myOwnCroupieWithBlackJack == null) {
@@ -31,160 +31,143 @@ public class Croupie {
         return myOwnCroupieWithBlackJack; // and whores
     }
 
-    
-    //  list of bets for next Spin
+    // list of bets for next Spin
     private List<Bet> bets = new ArrayList<Bet>();
-    
-    //  map of registered players
+
+    // map of registered players
     private Map<UUID, Player> players = new HashMap<UUID, Player>();
-        
-    //  binding bet to player
+
+    // binding bet to player
     private Map<UUID, UUID> betsToPlayer = new HashMap<UUID, UUID>();
-    
-    //  Player BetCodes Set
-    private Map< UUID, Set<Integer> > playerBets = new HashMap< UUID, Set<Integer> > ();
-    
-    
-    public OperationResult registerPlayer(Player player)
-    {	
+
+    // Player BetCodes Set
+    private Map<UUID, Set<Integer>> playerBets = new HashMap<UUID, Set<Integer>>();
+
+    public OperationResult registerPlayer(Player player) {
         OperationResult res = OperationResult.PLAYER_ALREADY_REGISTERED;
         if (!players.containsKey(player.getId())) {
             players.put(player.getId(), player);
-            
+
             // Create and Assign Set of BetCodes
             Set<Integer> betCodesSet = new HashSet<Integer>();
-            
+
             playerBets.put(player.getId(), betCodesSet);
-            
+
             res = OperationResult.PLAYER_REGISTERED;
         }
         return res;
     }
-    
-    public OperationResult registerBet(Bet bet, UUID playerId)
-    {
-		//  - check player is registered
-        if (!players.containsKey(playerId))
-        {
+
+    public OperationResult registerBet(Bet bet, UUID playerId) {
+        // - check player is registered
+        if (!players.containsKey(playerId)) {
             throw new NullPointerException("Player with ID=" + playerId + " is not registered");
         }
-        
-        //	- check bet unique for player
-        if (isBetNewForPlayer (bet, playerId) ) {
-    		addBet(bet, playerId);
-    		return OperationResult.BET_OK;
-    	}
-    	//	else
-    	return OperationResult.BET_WRONG;
+
+        // - check bet unique for player
+        if (isBetNewForPlayer(bet, playerId)) {
+            addBet(bet, playerId);
+            return OperationResult.BET_OK;
+        }
+        // else
+        return OperationResult.BET_WRONG;
     }
 
-    
-    private boolean isBetNewForPlayer(Bet bet, UUID playerId)
-    {
-    	Set<Integer> betCodesSet = playerBets.get(playerId);
-    	Integer newBetCode = bet.getBetCode();
-    	boolean betExist = betCodesSet.contains(newBetCode);
-    	boolean betIsNew = !betExist;
-    	
-    	return betIsNew;
-	}
+    private boolean isBetNewForPlayer(Bet bet, UUID playerId) {
+        Set<Integer> betCodesSet = playerBets.get(playerId);
+        Integer newBetCode = bet.getBetCode();
+        boolean betExist = betCodesSet.contains(newBetCode);
+        boolean betIsNew = !betExist;
 
-	public void flushAllPlayers ()
-    {
+        return betIsNew;
+    }
+
+    public void flushAllPlayers() {
         players.clear();
         flushAllBets();
     }
 
-	private void flushAllBets() {
-		bets.clear();
+    private void flushAllBets() {
+        bets.clear();
         betsToPlayer.clear();
         playerBets.clear();
-	}   
-    
-    
-    protected void addBet(Bet bet, UUID playerId)
-    {
-    	UUID betID = bet.getId();
-    	Integer betCode = bet.getBetCode();
-    	
+    }
+
+    protected void addBet(Bet bet, UUID playerId) {
+        UUID betID = bet.getId();
+        Integer betCode = bet.getBetCode();
+
         bets.add(bet);
         betsToPlayer.put(betID, playerId);
         playerBets.get(playerId).add(betCode);
-        
+
         players.get(playerId).incrementBetCount();
     }
-    
-    
-    private boolean enableManualSpin;   
-    
-    private boolean isManualSpin()
-    {
+
+    private boolean enableManualSpin;
+
+    private boolean isManualSpin() {
         return enableManualSpin;
     }
 
-    public void setManualSpin(boolean manualSpin, Integer number)
-    {
+    public void setManualSpin(boolean manualSpin, Integer number) {
         this.enableManualSpin = manualSpin;
         manualSpinNumber = number;
     }
 
     public boolean isPasswordValidForUserId(String userid, String password) {
-        if (players.containsKey(UUID.fromString(userid)))
-        {
+        if (players.containsKey(UUID.fromString(userid))) {
             return players.get(UUID.fromString(userid)).isPasswordOk(password);
         }
         return false;
     }
 
     public void checkPlayerExists(String name) throws ValidationException {
-        for (Player aPlayer : players.values())
-        {
-            if (aPlayer.getName().equals(name) )
-            {
+        for (Player aPlayer : players.values()) {
+            if (aPlayer.getName().equals(name)) {
                 throw new ValidationException("The player with name " + name + " already exists. Please choose another name");
             }
         }
-        
+
     }
-    
-    
-    
-    public void performGameMove () {
-  	
-//    	select win number
-    	int winNumber = isManualSpin() ?
-    			manualSpinNumber :
-    			roulette.performSpin();
-    	
-//    	proceed all bets
-    	for (Bet currentBet : bets) {
-			
-//    		calculate win/loose coins
-    		int betResult = currentBet.calcBetResult(winNumber);
-    		
- //    		store bet player
-    		UUID currentPlayerId = betsToPlayer.get(currentBet.getId());
-    		Player currentPlayer = players.get(currentPlayerId);
-    		    		
-    		currentPlayer.applyBetResult(betResult);
-    		
-    		boolean betWins = betResult > 0;
-    		if (betWins) {
-    			currentPlayer.incrementWinBetCount();
-    		}
-		}
-    	
-//    	clear bet arrays
-    	flushAllBets();
-//    	? betCodes.clear
-    	System.out.println("Spin performed, manual is " + isManualSpin());
+
+    public void performGameMove() {
+        // select win number
+        int winNumber = 0;
+
+        // winNumber = isManualSpin() ? manualSpinNumber : roulette.performSpin();
+        if (enableManualSpin) {
+            if (manualSpinNumber != null) {
+                winNumber = manualSpinNumber;
+            } else
+                winNumber = roulette.performSpin();
+        } else {
+            winNumber = roulette.performSpin();
+        }
+
+        // proceed all bets
+        for (Bet currentBet : bets) {
+
+            // calculate win/loose coins
+            int betResult = currentBet.calcBetResult(winNumber);
+
+            // store bet player
+            UUID currentPlayerId = betsToPlayer.get(currentBet.getId());
+            Player currentPlayer = players.get(currentPlayerId);
+
+            currentPlayer.applyBetResult(betResult);
+
+            boolean betWins = betResult > 0;
+            if (betWins) {
+                currentPlayer.incrementWinBetCount();
+            }
+        }
+
+        // clear bet arrays
+        flushAllBets();
+        // ? betCodes.clear
+        System.out.println("Spin performed, manual is " + isManualSpin());
+
     }
-    
-    
+
 }
-
-
-
-
-
-
